8c09b0bb289f82d23a4ba161298711223b5d8f9a
Jesse Kaukonen <jesse.kaukonen@delicode.fi>
2016-12-08 11:15:52 +0200

    update background image for dmg

---
+++ MacWindowInitializer.h
@@ -0,0 +1,11 @@
+#ifndef MACWINDOWINITIALIZER_H
+#define MACWINDOWINITIALIZER_H
+
+
+class MacWindowInitializer
+{
+public:
+    MacWindowInitializer();
+};
+
+#endif // MACWINDOWINITIALIZER_H
---
+++ MacWindowInitializer.mm
@@ -0,0 +1,6 @@
+#include "MacWindowInitializer.h"
+
+MacWindowInitializer::MacWindowInitializer()
+{
+
+}
